86723e21457bbe2f0c90a0557499dae8ca57bb9b
alisher-nil <alisher.nil@gmail.com>
2024-09-14 21:14:52 +0500

    feat: testing library capabilities

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-import telethon
+import asyncio
+import os
 
-print(telethon.__version__)
+from dotenv import load_dotenv
+from telethon import TelegramClient
+
+load_dotenv()
+
+api_id = os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
+
+
+async def main():
+    async with TelegramClient("anon", api_id, api_hash) as client:
+        await client.send_message("me", "Hello, myself!")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
