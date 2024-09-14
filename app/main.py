ec91551368eabb70b3383dda900ad5052fb16737
alisher-nil <alisher.nil@gmail.com>
2024-09-14 22:03:33 +0500

    Refactor logging configuration and add event handler for new messages

--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
-import asyncio
+import logging
 import os
 
 from dotenv import load_dotenv
-from telethon import TelegramClient
+from icecream import ic  # noqa: F401
+from telethon import TelegramClient, events
 
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.WARNING,
+)
 load_dotenv()
 
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
+client = TelegramClient("dev", api_id, api_hash)
 
 
-async def main():
-    async with TelegramClient("anon", api_id, api_hash) as client:
-        await client.send_message("me", "Hello, myself!")
+@client.on(events.NewMessage)
+async def counter(event):
+    pass
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+client.start()
+client.run_until_disconnected()
