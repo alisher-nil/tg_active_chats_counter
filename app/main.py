d26b01f6d3720b9ec1275abb368d07dd6cc4b8df
alisher-nil <alisher.nil@gmail.com>
2024-09-15 02:02:12 +0500

    Refactor logging configuration and add event handler for new messages

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from icecream import ic  # noqa: F401
 from telethon import TelegramClient, events
+from telethon.types import Channel, Chat, User
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
@@ -11,15 +12,30 @@ logging.basicConfig(
 )
 load_dotenv()
 
-api_id = os.getenv("API_ID")
-api_hash = os.getenv("API_HASH")
-client = TelegramClient("dev", api_id, api_hash)
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+USER_PASSWORD = os.getenv("USER_PASSWORD")
+USER_PHONE = os.getenv("USER_PHONE")
+client = TelegramClient("dev", API_ID, API_HASH)
 
 
-@client.on(events.NewMessage)
-async def counter(event):
-    pass
+@client.on(events.NewMessage(incoming=True))
+async def counter(event: events.NewMessage.Event):
+    print("Incoming message")
+
+    if event.is_private:
+        from_user: User = await event.get_chat()
+        print(
+            f"Private message from {from_user.first_name} "
+            f"{from_user.last_name} ({from_user.id})"
+        )
+    else:
+        from_channel: Channel = await event.get_chat()
+        print(
+            f"Group message in channel {from_channel.title} ({from_channel.id})",
+            event.chat_id,
+        )
 
 
-client.start()
+client.start(USER_PHONE, USER_PASSWORD)
 client.run_until_disconnected()
