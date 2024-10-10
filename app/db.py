4e2bc70f3db5a6159276995495b79c0d8ed255c6
alisher-nil <alisher.nil@gmail.com>
2024-10-10 16:49:28 +0500

    Add Base class for sqlalchemy models

--- app/db.py
+++ app/db.py
@@ -1,9 +1,24 @@
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    declared_attr,
+    mapped_column,
+    sessionmaker,
+)
 
 from config import settings
+
+
+class Base(DeclarativeBase):
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
 
 engine = create_async_engine(settings.database_url)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
