4b320095b4c17c40aa1430d8b9bd6c4ad4f76604
alisher-nil <alisher.nil@gmail.com>
2024-10-10 14:14:29 +0500

    Refactor new_user_handler in handlers.py to only print event details for private messages

--- app/handlers.py
+++ app/handlers.py
@@ -5,4 +5,8 @@ from client import client
 
 @client.on(NewMessage(incoming=True, forwards=False))
 async def new_user_handler(event: NewMessage.Event):
-    print("bar")
+    if not event.is_private:
+        return
+
+    print(event.stringify())
+    print(type(event))
