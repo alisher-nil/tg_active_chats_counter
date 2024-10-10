aa40f8536412c275a6acdbb923bfb34af17a5012
alisher-nil <alisher.nil@gmail.com>
2024-10-10 21:09:24 +0500

    create base.py for alembic models discovery

---
+++ app/base.py
@@ -0,0 +1,8 @@
+"""
+This file exists for alembic to see
+all the models that are derived from Base.
+All new models should be imported here.
+"""
+
+from app.core.db import Base
+from app.models import Users
