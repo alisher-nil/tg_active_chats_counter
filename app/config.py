fadbd2f63521f42cffa03a5aab860b718dbf33a8
alisher-nil <alisher.nil@gmail.com>
2024-10-12 23:28:54 +0500

    Update api_id field type to int in config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    api_id: str = Field(...)
+    api_id: int = Field(...)
     api_hash: str = Field(...)
     user_password: str | None = Field(None)
     user_phone: str = Field(...)
